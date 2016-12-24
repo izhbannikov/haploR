32198e044ecb210e77012c1f50d9c858015e6e43
izhbannikov <zhbannikov.ilya@gmail.com>
2016-12-24 00:58:02 -0500

    Version 1.1.0 released. Now the program supports a vector of SNPs on input.

--- R/query.R
+++ R/query.R
@@ -1,41 +1,63 @@
 #' This function queries Haploreg web-based tool and returns results.
 #' 
-#' @param query Variant (SNP) code, only one is supported now.
+#' @param query Query (a vector of rsIDs).
 #' @param ldThresh LD threshold, r2 (select NA to only show query variants). 
 #' Default: 0.8.
 #' @param ldPop 1000G Phase 1 population for LD calculation. 
 #' Can be: "AFR", "AMR", "ASN". Default: "EUR".
 #' @param epi Source for epigenomes. 
-#' Default: "ChromHMM (Core 15-state model)".
+#' Possible values: \code{vanilla} for ChromHMM (Core 15-state model);
+#' \code{imputed} for ChromHMM (25-state model using 12 imputed marks);
+#' \code{methyl} for H3K4me1/H3K4me3 peaks;
+#' \code{acetyl} for H3K27ac/H3K9ac peaks.
+#' Default: \code{vanilla}.
 #' @param cons Mammalian conservation algorithm. 
-#' Default: "SiPhy-omega".
-#' @param output Output mode. Text by default.
+#' Possible values: \code{gerp} for GERP,
+#' \code{siphy} for SiPhy-omega,
+#' \code{both} for both.
+#' Default: \code{siphy}.
+#' @param genetypes Show position relative to.
+#' Possible values: \code{gencode} for Gencode genes;
+#' \code{refseq} for RefSeq genes;
+#' \code{both} for both.
+#' Default: \code{gencode}.
 #' @param url Haplotype url address. 
 #' Default: "http://archive.broadinstitute.org/mammals/haploreg/haploreg.php"
 #' @param verbose Verbosing output. Default: FALSE.
 #' @return A data frame with results.
 #' @examples
-#' data <- queryHaploreg("rs10048158")
+#' data <- queryHaploreg(c("rs10048158","rs4791078"))
 #' head(data)
 #' @rdname haploR-methods
 #' @export
-queryHaploreg <- function(query, ldThresh=0.8, ldPop="EUR", 
-                    epi="ChromHMM (Core 15-state model)", 
-                    cons="siphy", output="text",
-                    url="http://archive.broadinstitute.org/mammals/haploreg/haploreg.php",
-                    verbose=FALSE) {
+queryHaploreg <- function(query, 
+                          ldThresh=0.8, 
+                          ldPop="EUR", 
+                          epi="vanilla", 
+                          cons="siphy", 
+                          genetypes="gencode",
+                          url="http://archive.broadinstitute.org/mammals/haploreg/haploreg.php",
+                          verbose=FALSE) {
     
-    body <- list(query = query, ldThresh=as.character(ldThresh), 
-                 ldPop=ldPop, epi=epi, 
-                 cons=cons, output=output)
   
-    # Form encoded
-    # Multipart encoded
+    trunc <- 1000 # can be 0 2 3 4 5 1000
+    oligo <- 1000 # can be 1, 6, 1000
+    output <- "text"
+    
+    body <- list(query = paste(query, collapse = ','), 
+                 ldThresh=as.character(ldThresh), 
+                 ldPop=ldPop, epi=epi, 
+                 cons=cons, 
+                 genetypes=genetypes,
+                 trunc=as.character(trunc),
+                 oligo=as.character(oligo),
+                 output=output)
+  
+    # Form encoded: multipart encoded
     r <- POST(url, body = body, encode="multipart")
   
-    bin <- content(r, "raw")
-    data.bin <- readBin(bin, character())
-    sp <- strsplit(data.bin, '\n')
+    dat <- content(r, "text")
+    sp <- strsplit(dat, '\n')
     res.table <- data.frame(matrix(nrow=length(sp[[1]])-1, ncol=length(strsplit(sp[[1]][1], '\t')[[1]])))
     colnames(res.table) <- strsplit(sp[[1]][1], '\t')[[1]]
   
